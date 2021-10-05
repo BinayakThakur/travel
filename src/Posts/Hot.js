5f7fcfa9de00427e359d96f04e14593e845a1f55
Binayak Thakur <thakurganesh919@gmail.com>
2021-10-05 20:10:19 +0530

    added HotData

--- src/Posts/Hot.js
+++ src/Posts/Hot.js
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { DataContext } from "../datacontext";
 
 const Hot = () =>{
-
+    const {hotData} =useContext(DataContext);
+    const {setHot}=useContext(DataContext);
     useEffect(
         ()=>{
          fetch("https://react-rest-spring.herokuapp.com/getPack")
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => setHot(data))
+        .catch((err)=>console.log(err));
          
         }
-    )
-
+    ,[setHot])
+        console.log(hotData);
 return (<>
 
     
