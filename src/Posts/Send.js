1cc7e52b234206a7290e88e5eec4f5c8ab06c1fc
Binayak Thakur <thakurganesh919@gmail.com>
2021-10-10 17:11:34 +0530

    fixed bugs

--- src/Posts/Send.js
+++ src/Posts/Send.js
@@ -15,8 +15,9 @@ const Send = () =>{
         setBody(e);
     }
     const {loggedUser}=useContext(DataContext);
+    const author=loggedUser+"";
     function send(){
-        fetch("https://react-rest-spring.herokuapp.com/post?place="+Heading+"&author="+{loggedUser}+"&comments="+Body+"&time="+(new Date().toLocaleString()))
+        fetch("https://react-rest-spring.herokuapp.com/post?place="+Heading+"&author="+author+"&comments="+Body+"&time="+(new Date().toLocaleString()))
         .then(setFeed(
 
             <Alert severity="success" className="ms-4">Posted!</Alert>
